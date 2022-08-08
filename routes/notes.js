d4e6d08903530f0d7b087f1b5adcceddc90b4aab
Jamlit37 <james.v.little@hotmail.com>
2022-08-08 18:03:34 +1000

    Working application with main changes added to routes/notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,45 @@
-const notesRouter = require('express').Router();
-const fs = require('fs');
+const fs = require("fs");
+const notesRouter = require("express").Router();
+const db = "./db/db.json"
 
-notesRouter.get('/', (req, res) =>
-  readFromFile('./db/db.json')
-  .then((data) => res.json(JSON.parse(data)))
-);
+notesRouter.get("/", (req, res) => {
+    const notesUpdated = JSON.parse(fs.readFileSync(db));
+    return res.json(notesUpdated);
+});
 
+notesRouter.post("/", (req, res) => {
+    const newNote = req.body;
+    const currentNotes = fs.readFileSync(db);
+    let notesList = JSON.parse(currentNotes);
+
+    let idsCurrent = [];
+    let idsNew = 0;
+    let idsUnique = false;
+
+    for (notes of notesList) {
+        idsCurrent.push(parseInt(notes.id));
+    }
+
+    while (!idsUnique) {
+        idsNew++;
+        if (!idsCurrent.includes(idsNew)) {
+            idsUnique = true;
+        }
+    }
+    newNote.id = idsNew;
+    notesList.push(newNote);
+    fs.writeFileSync(db, JSON.stringify(notesList));
+    res.json();
+});
+
+notesRouter.delete("/:id", (req, res) => {
+    const notesCurrent = fs.readFileSync(db);
+    const notesID = req.params.id;
+    let notesList = JSON.parse(notesCurrent);
+
+    notesList = notesList.filter((note) => note.id != notesID);
+    fs.writeFileSync(db, JSON.stringify(notesList));
+    res.json();
+});
+
+module.exports = notesRouter;
